1bec12fdd132d8b333113e388e688d902a7b3d8e
s.lugovskiy <s.lugovskiy@ws-076.isp.local>
2014-12-29 15:46:47 +0300

    fix

--- tests/acceptance/DashboardCest.php
+++ tests/acceptance/DashboardCest.php
@@ -10,14 +10,16 @@ class DashboardCest {
 
     public function _before(AcceptanceTester $I) {
        $this->page = new FrontPage($I);
+       $this->page->openMainPage();
     }
 
     public function _after(AcceptanceTester $I) {
-        
+       $this->page->openMainPage();
     }
 
     public function loginTest(AcceptanceTester $I) {
         $this->page->login();
     }
+ 
 
 }
--- tests/acceptance/pages/FrontPage.php
+++ tests/acceptance/pages/FrontPage.php
@@ -12,39 +12,55 @@
  */
 class FrontPage  {
     
-    private $URL = '/ru/';
+    private $URL = '/';
+    private $dashboardUrl = '/webmaster/office/dashboard';
     private $loginLink = "//a[@id='popup-login-link' and ancestor::div[@class='link relative' and ancestor::li[@class='login']]]";
     private $popup = " //div[@id='popup-login']";
     private $nick = "//input[@name='nick']";
     private $password = "//input[@name='passwd']";
     private $submit = "//span[@class='submit' and ancestor::form[ancestor::div[@class='popup-padding']]]";
     private $overview = "//h2[contains(@class,'_overview_text')]";
+    private $spinner = "//div[@role='spinner']";
+    
+
     
     /**
      * @var AcceptanceTester
      */
-    protected $AcceptanceTester;
+    private $I;
     
     public function __construct(AcceptanceTester $I)
     {
-        $this->AcceptanceTester = $I;
+        $this->I = $I;
     }
     
     public function login()
     {
-        $I = $this->AcceptanceTester;
-        
-        $I->wantTo("login");
-        $I->amOnPage($this->URL);
-        $I->click($this->loginLink);
-        $I->canSeeElement($this->popup);
-        $I->fillField($this->nick, 'me0i@mail.ru');
-        $I->fillField($this->password, '123698745');
-        $I->click($this->submit);
-        $I->waitForElement($this->overview,30);
-        $I->seeElement($this->overview);
-
-    }    
+        $this->I->wantTo("login");
+        $this->I->click($this->loginLink);
+        $this->I->canSeeElement($this->popup);
+        $this->I->fillField($this->nick, 'me0i@mail.ru');
+        $this->I->fillField($this->password, '123698745');
+        $this->I->click($this->submit);
+        $this->I->waitForElement($this->overview,30);
+        $this->waitSpinner();
+        $this->I->canSeeElement($this->overview);
+    }
+ 
+    
+    public function openMainPage(){
+        $this->I->amOnPage($this->URL);
+    }
+    
+    public function goToDashboard(){
+        $this->I->amOnPage($this->dashboardUrl);
+        $this->I->waitForElement($this->overview,30);
+        $this->waitSpinner();
+    }
+    
+    public function waitSpinner(){
+        $this->I->waitForElementNotVisible($this->spinner);
+    }
     
 }
 
